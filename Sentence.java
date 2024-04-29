53951b50a91714c0d8a93420a70576aea36b18a1
Faisal N <contact@faisaln.com>
2024-04-29 00:28:18 -0400

    Finish Sentence.java

--- Sentence.java
+++ Sentence.java
@@ -5,36 +5,49 @@ public class Sentence {
 
     public ArrayList<Integer> getBlankPositions() {
         ArrayList<Integer> blankPositions = new ArrayList<Integer>();
-        int i;
-        for (i = 0; i < sentence.length() - 1; i++) {
-            if (sentence.substring(i, i + 1) == " ")
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.substring(i, i + 1).equals(" "))
                 blankPositions.add(i);
         }
         return blankPositions;
     }
 
     public int countWords() {
-        return 5;
+        return getBlankPositions().size() + 1;
     }
 
     public String[] getWords() {
-        String[] words = new String[0];
         int wordCount = countWords();
+        String[] words = new String[wordCount];
         ArrayList<Integer> blankPositions = getBlankPositions();
         int i;
-        for (i = 0; i < wordCount; i++) {
-            int start = 0;
-            if (i > 0) start = blankPositions.get(i-1);
-            String word = sentence.substring(start, blankPositions.get(i));
-            words[i] = word;
-        }
         if (blankPositions.size() == 0 && wordCount == 1) {
             words[0] = sentence;
+        } else {
+            for (i = 0; i < wordCount; i++) {
+                int start = 0;
+                if (i > 0)
+                    start = blankPositions.get(i - 1) + 1;
+                if (blankPositions.size() == i) {
+                    String word = sentence.substring(start);
+                    words[i] = word;
+                } else {
+                    String word = sentence.substring(start, blankPositions.get(i));
+                    words[i] = word;
+                }
+            }
         }
         return words;
     }
 
     public static void main(String[] args) {
-
+        Sentence sentence1 = new Sentence();
+        sentence1.sentence = "The bird flew away!";
+        System.out.println(sentence1.getBlankPositions());
+        System.out.println(sentence1.countWords());
+        int i;
+        for (i = 0; i < sentence1.countWords(); i++) {
+            System.out.println(sentence1.getWords()[i]);
+        }
     }
 }
